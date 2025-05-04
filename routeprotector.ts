5823a0635f246d483539266a30c3aa8cef9a27cd
ViVcmd <vinzent.ga@sser.ch>
2025-05-04 17:04:09 +0200

    Create routeprotector.ts

---
+++ routeprotector.ts
@@ -0,0 +1,10 @@
+const isAuthenticated = (req: any, res: any, next: any) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/auth/google');
+};
+
+app.get('/protected', isAuthenticated, (req, res) => {
+  res.send('This is a protected route, accessible only to authenticated users.');
+});
