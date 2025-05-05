3a107677e9c6045e22eaca317f949cd86fbcc2d7
ViVcmd <vinzent.ga@sser.ch>
2025-05-05 16:50:31 +0200

    Update routeprotector.ts

--- routeprotector.ts
+++ routeprotector.ts
@@ -2,7 +2,7 @@ const isAuthenticated = (req: any, res: any, next: any) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/auth/google');
+  res.redirect('/auth/google'); // Redirect to Google OAuth2
 };
 
 app.get('/protected', isAuthenticated, (req, res) => {
